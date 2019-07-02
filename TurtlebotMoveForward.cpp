7f005dc031c26f63135aa0db36be5f2d15716cd1
MariaC27 <mariasoccer2123@gmail.com>
2019-07-02 12:53:14 -0400

    two points framework modified

--- TurtlebotMoveForward.cpp
+++ TurtlebotMoveForward.cpp
@@ -6,33 +6,53 @@
 #include <actionlib/server/simple_action_server.h>
 #include "geometry_msgs/PoseWithCovarianceStamped.h"
 #include "geometry_msgs/PointStamped.h" 
-ros::Publisher velocity_publisher; 
-ros::Subscriber pose_subscriber;
-turtlesim::Pose turtlesim_pose;
 
+// Function declarations 
+void update_endpoints(const geometry_msgs::PointStamped::ConstPtr& msg); 
+void move_turtlebot(double x, double y, double yaw); 
+void callback(const geometry_msgs::PoseWithCovarianceStamped::ConstPtr& msg); 
 
-//method to move the robot straight forward
+// Global variables for the two endpoints 
+double first_x = -1;
+double first_y = -1;
+double second_x = -1;
+double second_y = -1; 
+int seq_counter = 0; 
 
-void move(double speed, double distance, bool isForward);
+int main(int argc, char **argv)
+{ 
+	ros::init(argc, argv, "patrolling_turtle"); 
+	ros::NodeHandle n;
 
-int main (int argc, char **argv){
-    ros::init(argc, argv, "turtle_move_forward");
-    ros::NodeHandle n;
-    
+	// Subscribes to two topics
+	ros::Subscriber sub = n.subscribe("test1", 1000, update_endpoints); 
 
-    velocity_publisher = n.advertise<geometry_msgs::Twist>("/turtle1/cmd_vel", 10);
-
-    ros::Rate loop_rate(10);
-
-    ROS_INFO("\n\n\n******STARTING******\n");
-    
-    move_turtlebot(2.0, 2.0, 0.0);
-
-    ros::spin();
-
-    return 0;
+	ros::Subscriber sub2 = n.subscribe("test2", 1000, callback); 
+	
+	ros::spin();
 }
 
+// Name: update_endpoints
+// Parameters: pointer to geometry_msgs::PointStamped
+// Does: updates the global points between which the robot will patrol
+//	and once they are declared, sends the robot to the first one
+void update_endpoints(const geometry_msgs::PointStamped::ConstPtr& msg)
+{ 
+
+	
+
+		ROS_INFO("Thank you! Beginning patrol now...\n"); 
+		move_turtlebot(first_x, first_y, 0.0); 
+		
+	
+
+}
+
+// Name: move_turtlebot
+// Parameters: x,y coordinates and an angle
+// Does: sends a message to move the robot to the given x,y spot and 
+//	face in the given direction
+// Note: assumes a 2-D grid, thus z is always 0
 void move_turtlebot(double x, double y, double yaw)
 {
 	actionlib::SimpleActionClient<move_base_msgs::MoveBaseAction> ac("move_base", true);
@@ -50,4 +70,16 @@ void move_turtlebot(double x, double y, double yaw)
 		ac.sendGoal(goal);
 		ac.waitForResult(); 
 
+}
+
+// Name: callback
+// Parameters: pointer to geometry_msgs::PoseWithCovarianceStamped
+// Does: checks to see which point the robot is closest to and then goes to the opposite point
+// Note: currently all orientations are set to 0.0, but that is arbitrary 
+void callback(const geometry_msgs::PoseWithCovarianceStamped::ConstPtr& msg)
+{
+		
+			move_turtlebot(first_x, first_y, 0.0); 
+		
+		
 }
