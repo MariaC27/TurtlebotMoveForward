95ace9b69cbd50a954b5246c424d67e85b3e760e
MariaC27 <mariasoccer2123@gmail.com>
2019-07-02 15:51:13 -0400

    working code

--- TurtlebotMoveForward.cpp
+++ TurtlebotMoveForward.cpp
@@ -24,29 +24,13 @@ int main(int argc, char **argv)
 	ros::init(argc, argv, "patrolling_turtle"); 
 	ros::NodeHandle n;
 
-	// Subscribes to two topics
-	ros::Subscriber sub = n.subscribe("test1", 1000, update_endpoints); 
-
-	ros::Subscriber sub2 = n.subscribe("test2", 1000, callback); 
 	
+    move_turtlebot(5.8, 13.7, 0.0);
+    //get these points by going to map and selecting a point 
+
 	ros::spin();
 }
 
-// Name: update_endpoints
-// Parameters: pointer to geometry_msgs::PointStamped
-// Does: updates the global points between which the robot will patrol
-//	and once they are declared, sends the robot to the first one
-void update_endpoints(const geometry_msgs::PointStamped::ConstPtr& msg)
-{ 
-
-	
-
-		ROS_INFO("Thank you! Beginning patrol now...\n"); 
-		move_turtlebot(first_x, first_y, 0.0); 
-		
-	
-
-}
 
 // Name: move_turtlebot
 // Parameters: x,y coordinates and an angle
@@ -72,14 +56,3 @@ void move_turtlebot(double x, double y, double yaw)
 
 }
 
-// Name: callback
-// Parameters: pointer to geometry_msgs::PoseWithCovarianceStamped
-// Does: checks to see which point the robot is closest to and then goes to the opposite point
-// Note: currently all orientations are set to 0.0, but that is arbitrary 
-void callback(const geometry_msgs::PoseWithCovarianceStamped::ConstPtr& msg)
-{
-		
-			move_turtlebot(first_x, first_y, 0.0); 
-		
-		
-}
